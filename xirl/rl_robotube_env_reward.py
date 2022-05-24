ee705252cf2a3a0f4e26727bbcd6ea03b379f3b8
Simon-Fuhaoyuan <Simon-Fuhaoyuan@sjtu.edu.cn>
2022-05-24 11:23:08 +0800

    update rl scripts

--- xirl/rl_robotube_env_reward.py
+++ xirl/rl_robotube_env_reward.py
@@ -1,6 +1,7 @@
 """RoboTube: Train a policy with the sparse environment reward."""
 
 
+import subprocess
 from absl import app
 from absl import flags
 from absl import logging
@@ -8,16 +9,18 @@ from torchkit.experiment import string_from_kwargs
 from torchkit.experiment import unique_id
 from utils import get_time_str
 import os
+import re
 
 
 # pylint: disable=logging-fstring-interpolation
 
 FLAGS = flags.FLAGS
-CONFIG_PATH = "configs/robotube/drawer_closing.py"
+CONFIG_PATH = "configs/robotube"
 
 flags.DEFINE_string("env", None, "Which environment to train.")
 flags.DEFINE_string("device", "cuda:0", "The compute device.")
 flags.DEFINE_list("seeds", [0], "List specifying the seeds to run.")
+flags.DEFINE_bool("parallel", False, "Whether run program in parallel version")
 
 
 def main(_):
@@ -28,32 +31,44 @@ def main(_):
     experiment_name = env_name + '_env_' + get_time_str()
     logging.info("Experiment name: %s", experiment_name)
 
-    for seed in FLAGS.seeds:
-        command = "python train_policy.py --experiment_name " + experiment_name + " --env_name " + f"{env_name}" + \
-                  " --config " + f"{CONFIG_PATH}" + " --seed " + f"{seed}" + " --device " + f"{FLAGS.device}"
-        os.system(command)
+    # Parse env name and corresponding config file
+    env_name_split_list = re.findall('[A-Z][^A-Z]*', env_name)
+    config_file = env_name_split_list[0].lower() + '_' + env_name_split_list[1].lower() + '.py'
+    config_file = os.path.join(CONFIG_PATH, config_file)
 
-    # Execute each seed in parallel.
-    # procs = []
-    # procs.append(
-    #     subprocess.Popen([  # pylint: disable=consider-using-with
-    #         "python",
-    #         "train_policy.py",
-    #         "--experiment_name",
-    #         experiment_name,
-    #         "--env_name",
-    #         f"{env_name}",
-    #         "--config",
-    #         f"{CONFIG_PATH}",
-    #         "--seed",
-    #         f"{FLAGS.seed}",
-    #         "--device",
-    #         f"{FLAGS.device}",
-    #     ]))
-    #
-    # # Wait for each seed to terminate.
-    # for p in procs:
-    #     p.wait()
+    if not FLAGS.parallel:
+        for seed in FLAGS.seeds:
+            command = "python train_policy.py"
+            command += " --experiment_name " + experiment_name
+            command += " --env_name " + f"{env_name}"
+            command += " --config " + f"{config_file}"
+            command += " --seed " + f"{seed}"
+            command += " --device " + f"{FLAGS.device}"
+            os.system(command)
+
+    else:
+        # Execute each seed in parallel.
+        procs = []
+        for seed in FLAGS.seeds:
+            procs.append(
+                subprocess.Popen([  # pylint: disable=consider-using-with
+                    "python",
+                    "train_policy.py",
+                    "--experiment_name",
+                    experiment_name,
+                    "--env_name",
+                    f"{env_name}",
+                    "--config",
+                    f"{CONFIG_PATH}",
+                    "--seed",
+                    f"{FLAGS.seed}",
+                    "--device",
+                    f"{FLAGS.device}",
+                ]))
+
+        # Wait for each seed to terminate.
+        for p in procs:
+            p.wait()
 
 
 if __name__ == "__main__":
--- xirl/rl_robotube_learned_reward.py
+++ xirl/rl_robotube_learned_reward.py
@@ -9,6 +9,7 @@ from torchkit.experiment import unique_id
 from utils import get_time_str
 import os
 import pyrfuniverse.assets as assets
+import re
 
 
 # pylint: disable=logging-fstring-interpolation
@@ -17,14 +18,14 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string("env", None, "Which environment to train.")
 flags.DEFINE_string("device", "cuda:0", "The compute device.")
 flags.DEFINE_list("seeds", [0], "List specifying the seeds to run.")
-flags.DEFINE_string("pretrain", None, "large or small")
+flags.DEFINE_string("pretrain", None, "gc, tcn or xirl")
 flags.DEFINE_bool("parallel", False, "Whether run program in parallel version")
 
 
 def main(_):
     # Map the embodiment to the x-MAGICAL env name.
     env_name = FLAGS.env
-    CONFIG_PATH = "configs/robotube/drawer_closing_w_learned_reward.py"
+    CONFIG_PATH = "configs/robotube"
 
     PRETRAINED_PATH = assets.join_path('RoboTube/DrawerClosing/l-dod_models/' + FLAGS.pretrain)
     if FLAGS.pretrain == 'gc':
@@ -36,12 +37,17 @@ def main(_):
     experiment_name = env_name + '_' + FLAGS.pretrain + '_' + get_time_str()
     logging.info("Experiment name: %s", experiment_name)
 
+    # Parse env name and corresponding config file
+    env_name_split_list = re.findall('[A-Z][^A-Z]*', env_name)
+    config_file = env_name_split_list[0].lower() + '_' + env_name_split_list[1].lower() + '_w_learned_reward.py'
+    config_file = os.path.join(CONFIG_PATH, config_file)
+
     if not FLAGS.parallel:
         for seed in FLAGS.seeds:
             command = "python train_policy.py"
             command += " --experiment_name " + experiment_name
             command += " --env_name " + f"{env_name}"
-            command += " --config " + f"{CONFIG_PATH}"
+            command += " --config " + f"{config_file}"
             command += " --seed " + f"{seed}"
             command += " --device " + f"{FLAGS.device}"
             command += " --config.reward_wrapper.pretrained_path " + f"{PRETRAINED_PATH}"
