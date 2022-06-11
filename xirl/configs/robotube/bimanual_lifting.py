75bb8ba9127ea0ffd867bc2e2d5632e8d17bb56c
Simon-Fuhaoyuan <Simon-Fuhaoyuan@sjtu.edu.cn>
2022-06-12 01:56:24 +0800

    update config

--- xirl/configs/robotube/bimanual_lifting.py
+++ xirl/configs/robotube/bimanual_lifting.py
@@ -6,7 +6,7 @@ def get_config():
     config = _get_config()
 
     config.save_dir = "logs/"
-    config.num_train_steps = 500_000
+    config.num_train_steps = 1_000_000
     config.replay_buffer_capacity = 100_000
     config.num_seed_steps = 1000
     config.num_eval_episodes = 10
